d2d1d695203fa4d2b8777e3e305c82898b990315
Niomoo <jennyliu.lyh@gmail.com>
2020-12-27 18:21:59 +0800

    Remove private info

--- fsm.py
+++ fsm.py
@@ -399,27 +399,6 @@ class TocMachine(GraphMachine):
         resDetail = ">".join(detail)
         return str(resDetail)
 
-    GOOGLE_API_KEY = 'AIzaSyD1oQOM90IDt-hyKm-vKI9f4X2c_AAm-kY'
-
-    def get_latitude_longtitude(address):
-        # decode url
-        address = urllib.request.quote(address)
-        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + '&key=' + GOOGLE_API_KEY
-        
-        while True:
-            res = requests.get(url)
-            js = json.loads(res.text)
-
-            if js["status"] != "OVER_QUERY_LIMIT":
-                time.sleep(1)
-                break
-
-        result = js["results"][0]["geometry"]["location"]
-        lat = result["lat"]
-        lng = result["lng"]
-        print(lat,lng)
-        return lat, lng
-
     # 隨輸入更改之參數
     userInput = "高雄車站"       #使用者輸入關鍵字找出最近景點
     lng = float(125)        #定位使用者經度
